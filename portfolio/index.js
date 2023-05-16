bd1bda14006f69710775fc44c6ccbc53572daec1
SUBHANKARPARIA <91493158+SUBHANKARPARIA@users.noreply.github.com>
2023-05-17 01:53:05 +0530

    Add files via upload

---
+++ portfolio/index.js
@@ -0,0 +1,33 @@
+// const sections=document.querySelectorAll('section');
+// const navi= document.querySelectorAll('.navbar ul li');
+
+// window.addEventListener('scroll',()=>{
+//     let current='';
+
+//     sections.forEach(section =>{
+//         const sectiontop=section.offsetTop;
+//         const sectionheight=section.clientHeight;
+//         if(scrollY> sectiontop)
+//         {
+//             current=section.getAttribute('id');
+//         }
+//     })
+
+//     navi.forEach(li => {
+//         li.classList.remove('active');
+//         if(li.classList.contains(current))
+//         {
+//             li.classList.add('active')
+//         }
+//     })
+        
+//     });
+
+/*javascript for phone response*/
+
+// const togglebutton=document.getElementById('togglebutton');
+// const navilist= document.getElementById('navlist');
+
+// togglebutton.addEventListener('click', () =>{
+//     navilist.classList.toggle('active');
+// })
